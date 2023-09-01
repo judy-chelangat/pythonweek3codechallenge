5a7b250ea0463df0bceb44747080b843ed041f2e
judy-chelangat <judysigilai75@gmail.com>
2023-09-01 18:30:49 +0300

    added random data to the reviews and added session close

--- lib/seed.py
+++ lib/seed.py
@@ -3,7 +3,7 @@ import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Restuarant,Customer
+from models import Restuarant,Customer,Review
 
 
 fake = Faker()
@@ -13,8 +13,9 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///restuarants.db')
     Session = sessionmaker(bind=engine)
     session = Session()
-    session.query(Restuarant).delete() #to delete the previous recors to avoid duplication
+    session.query(Restuarant).delete() #to delete the previous records to avoid duplication
     session.query(Customer).delete()
+    session.query(Review).delete()
 
 #to check if its working 
 print("Seeding restuarants...")
@@ -36,6 +37,25 @@ customers =[
 ]
 
 #saving the restuarants,customers  to the database 
-session.bulk_save_objects(restuarants)
-session.bulk_save_objects(customers)
+session.add_all(restuarants)
+session.add_all(customers)
 session.commit()
+
+#adding records for the reviews
+print('seeding reviews')
+reviews = [
+        Review(
+              customer=random.choice(customers),  # Using  the actual Customer object
+              restuarant=random.choice(restuarants), #random choice from the restuarant
+              star_rating=random.randint(1, 5)
+        )
+        for k in range(10)  # Generating 10 reviews
+    ]
+
+
+#adding reviews to the session and commiting 
+session.add_all(reviews)
+session.commit()
+
+#closing the session
+session.close()
