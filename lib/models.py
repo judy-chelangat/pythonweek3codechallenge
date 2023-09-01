d3584064e404a217c808a0be8dfa329fc0c3d821
judy-chelangat <judysigilai75@gmail.com>
2023-09-01 14:58:43 +0300

    created the customer model table

--- lib/models.py
+++ lib/models.py
@@ -12,7 +12,7 @@ engine = create_engine(DATABASE_URI,echo=True)
 #base class for allthe classes
 Base =declarative_base()
 
-
+#restuarant table
 class Restuarant(Base):
     __tablename__='restuarants'
     restuarant_id=Column(Integer ,Sequence('resturant_id_seq'), primary_key=True)
@@ -24,3 +24,16 @@ def __repr__(self):
         return f"Restuarant {self.restuarant_id}: " \
             + f"{self.name}, " \
             + f"Price {self.price}"
+
+#customer table
+class Customer(Base):
+      __tablename__='customers'
+      customer_id=Column(Integer,Sequence('customer_id_seq'),primary_key=True)
+      first_name=Column(String)
+      last_name=Column(String)
+
+
+def __repr__(self):
+        return f"Customer {self.customer_id}: " \
+            + f"{self.first_name}, " \
+            + f"lastname {self.last_name}"
