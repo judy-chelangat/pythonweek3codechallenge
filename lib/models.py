3ff25ab3f86d9bf56aa4c785ebbfe15009aa288c
judy-chelangat <judysigilai75@gmail.com>
2023-09-01 14:02:39 +0300

    added restuarant table and using migrations to add it to the database

--- lib/models.py
+++ lib/models.py
@@ -1,8 +1,26 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Column,String,ForeignKey,Integer
+from sqlalchemy import Column,String,ForeignKey,Integer,Sequence
 
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///restuarants.db')
+# define the database connection 
+DATABASE_URI = 'sqlite:///restuarants.db' #path to the database
 
-Base =declarative_base
+engine = create_engine(DATABASE_URI,echo=True)
+ #creating the engine
+
+#base class for allthe classes
+Base =declarative_base()
+
+
+class Restuarant(Base):
+    __tablename__='restuarants'
+    restuarant_id=Column(Integer ,Sequence('resturant_id_seq'), primary_key=True)
+    name =Column(String)
+    price =Column(Integer)
+
+
+def __repr__(self):
+        return f"Restuarant {self.restuarant_id}: " \
+            + f"{self.name}, " \
+            + f"Price {self.price}"
